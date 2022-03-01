f9c9fec9db0ad914c25ffc5c123bb0dd37df7a80
roflanmen <bohdan7nava@gmail.com>
2022-03-01 16:02:31 +0200

    upd

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require("http");
 http.createServer((req, res) => {
-    console.log(JSON.stringify(req));
+    console.log(JSON.stringify(req.headers));
     res.end(req.headers.host);
 }).listen(process.env.PORT || 5000);
