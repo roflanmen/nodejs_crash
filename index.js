408579097b816ca331445ee08b0afe8079086bc6
roflanmen <bohdan7nava@gmail.com>
2022-03-01 16:05:12 +0200

    upd

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require("http");
 http.createServer((req, res) => {
-    console.log(JSON.stringify(req.headers));
-    res.end(req.headers.host);
+    console.log(JSON.stringify(req.headers["x-forwarded-for"]));
+    res.end();
 }).listen(process.env.PORT || 5000);
